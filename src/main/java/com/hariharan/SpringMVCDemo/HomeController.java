8c665103f9c745bc4668d2b199fcd5841475400e
Hariharan <hariharan9572@gmail.com>
2026-01-09 19:38:28 +0530

    Creating a spring mvc project

---
+++ src/main/java/com/hariharan/SpringMVCDemo/HomeController.java
@@ -0,0 +1,41 @@
+package com.hariharan.SpringMVCDemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @ModelAttribute("course")
+    public String courseName(){
+        return "Java";
+    }
+
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("home method called");
+        return "index";
+    }
+
+    @RequestMapping("add")
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv){
+
+        /*int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));*/
+        int result = num1+num2;
+
+        mv.addObject("result", result);
+        mv.setViewName("result");
+
+        return mv;
+    }
+
+    @RequestMapping("addAlien")
+    public String addAlien(Alien alien){
+        return "result";
+    }
+
+}
